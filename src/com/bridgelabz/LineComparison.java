31012e3167aaf92851fac67c2eae862727680cf3
Sakshi <sakshishetty1231997@gmail.com>
2021-08-05 17:42:33 +0530

    Added: Welcome message to the program

---
+++ src/com/bridgelabz/LineComparison.java
@@ -0,0 +1,12 @@
+package com.bridgelabz;
+/**
+ * @author Sakshi Shetty
+ * Purpose - To simulate Line Comparison and Computation Program
+ */
+
+public class LineComparison {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Line Comparison Computation Program");
+
+	}
+}
